47c4acb313fc16b018cff061857e8d74124f5728
Anastasiya777111 <avojtesik1@gmail.com>
2021-08-25 17:23:02 +0300

    feat:
    add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const openBr = bracketsConfig.map(value => value[0]);
+    const closeBr = bracketsConfig.map(value => value[1]);
+    const stack = [];
+  
+    for (let i of str) {
+    const openIndex = openBr.indexOf(i);
+      const closeIndex = closeBr.indexOf(i);
+      
+  
+      if (openIndex === closeIndex) {
+        if (stack[stack.length - 1] === openBr[openIndex]) {
+          stack.pop();
+        } else {
+          stack.push(openBr[openIndex]);
+        }
+        continue;
+      }
+  
+      if (openIndex >= 0) {
+        stack.push(openIndex);
+        continue;
+      }
+  
+      if (closeIndex !== stack.pop()) {
+        return false;
+      }
+    }
+  
+    return !stack.length;
+  
+  
+  }
